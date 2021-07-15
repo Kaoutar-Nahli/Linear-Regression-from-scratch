7522f57f407c2d85e1cd4f5f67bc12f54e4ef246
unknown <kautarnahli@hotmail.es>
2021-07-15 17:09:40 +0100

    shuffling the data make the peaks smoother but final loss is higher

--- linear_reg_from_scratch.py
+++ linear_reg_from_scratch.py
@@ -1,81 +1,10 @@
-# %%
-from sklearn import datasets
-import numpy as np
-import matplotlib.pyplot as plt
-np.random.seed(40)
-data = datasets.load_boston()
-data.keys()
-data['feature_names']
-X, y = datasets.load_boston(return_X_y=True)
-X_mean = np.mean(X, axis=0)
-X_std = np.std(X, axis=0)
-X = (X - X_mean) / X_std
 
-class Dataloader():
-    def __init__(self):
-        self.batches=[]
-    def divide(self,X,y, batch_size):
-        for i in range(len(X)//batch_size):
-            if i+batch_size<len(X):
-               self.batches.append([X[i:i+batch_size],y[i:i+batch_size]])
-            else:
-               self.batches.append([X[i:],y[i:]])  
-        return self.batches
-    
-class LinearRegression:
-    def __init__(self):
-        self.w= np.random.randn(X.shape[1])
-        self.b = np.random.randn()
-        self.list_mse_epochs = []
-        self.list_mse_batches = []
-
-    def fit(self,X, y) :
-        learning_rate = 0.001
-        dataloader = Dataloader()
-        dataloader = dataloader.divide(X,y,16)
-        print (dataloader[0][0].shape)
-        print (dataloader[0][1].shape)
-    
-        self.list_mse_epochs = []
-        self.list_mse_batches = []
-      
-        for i in range(20):
-            
-            for n in range(len(dataloader)):
-                x_batches = dataloader[n][0]
-                y_batches = dataloader[n][1]
-                y_hat = self.pred(x_batches)
-                grad_w = 2 * (y_hat - y_batches) @ x_batches
-                grad_b = 2 * np.mean(y_hat-y_batches)
-                self.w = self.w - learning_rate * grad_w
-                self.b = self.b - learning_rate * grad_b
-                self.list_mse_batches.append(self._mse(y_batches,y_hat))
-                #n=n+1
-                print(n, len(x_batches))
-            self.list_mse_epochs.append(self._mse(y_batches,y_hat))
-            print(self._mse(y_hat,y_batches) , 'epoch', i)
-       
-    #def shuffle ():
-        
-    def _mse(self, y, y_pred):
-        return np.mean((y-y_pred)**2)
-
-    def pred(self, X):
-        return X@self.w + self.b
-
-
-
-
-
-model =LinearRegression()
-model.fit(X,y)
-
-#plt.plot(range(len(model.list_mse_epochs)), model.list_mse_epochs, color='red')
-plt.plot(range(len(model.list_mse_batches)), model.list_mse_batches)
 #%%
 from sklearn import datasets
+import random
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.utils import shuffle
 np.random.seed(40)
 data = datasets.load_boston()
 data.keys()
@@ -113,6 +42,18 @@ class LinearRegression:
         self.list_mse_epochs = []
         self.list_mse_batches = []
 
+    def shuffle_data (self,bx, by):
+        """
+        This function only shuffles the array along the first axis of a multi-dimensional array.
+        The order of sub-arrays is changed but their contents remains the same.
+        """
+        w_and_b = np.c_[bx,by] # adds y to X as a column from 506,13 to 506,14
+        np.random.shuffle(w_and_b) # shuffle  returns a nontype, modifies the array I passed  
+        #split  previous combined arrays t X and y
+        X = w_and_b[:,:-1]
+        y = w_and_b[:,-1]
+        return X,y
+
     def fit(self,X, y) :
         learning_rate = 0.001
         #divide dataset in batches of 16 samples from the dataset
@@ -122,24 +63,28 @@ class LinearRegression:
         n = 0
         for i in range(20):
             for x_batches, y_batches in dataloader:
-                #prediction for the the batches using non optimal parameters during training 
+                #shuffle batches
+
+                x_batches_shuffled, y_batches_shuffled = self.shuffle_data(x_batches, y_batches)
+                #prediction for the the batches using non optimal parameters yet during training 
                 # to calculate the partial derivative descent and the loss function 
-                y_hat = self.pred(x_batches)
+                y_hat = self.pred(x_batches_shuffled)
                 # calculate the partial derivative
-                grad_w = 2 * (y_hat - y_batches) @ x_batches
-                grad_b = 2 * np.mean(y_hat-y_batches)
+                grad_w = 2 * (y_hat - y_batches_shuffled) @ x_batches_shuffled
+                grad_b = 2 * np.mean(y_hat-y_batches_shuffled)
                 #updating parameters 
                 self.w = self.w - learning_rate * grad_w
                 self.b = self.b - learning_rate * grad_b
                 #calculating and appending loss function for each batch 
                 n+=1
-                self.list_mse_batches.append(self._mse(y_batches,y_hat))
+                self.list_mse_batches.append(self._mse(y_batches_shuffled,y_hat))
                 print(n)
             #calculating and appending loss function for each epoch
             self.list_mse_epochs.append(self._mse(y_batches,y_hat))
             print(self._mse(y_hat,y_batches) , 'epoch', i)
        
-    #def shuffle ():
+    
+
         
     def _mse(self, y, y_pred):
         return np.mean((y-y_pred)**2)
@@ -155,4 +100,4 @@ model.fit(X,y)
 #plt.plot(range(len(model.list_mse_epochs)), model.list_mse_epochs, color='red')
 plt.plot(range(len(model.list_mse_batches)), model.list_mse_batches)
 
-# %%
+
