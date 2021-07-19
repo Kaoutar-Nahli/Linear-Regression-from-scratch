d862902d5e2e6ddb048322fa2d116693b1b13f5a
unknown <kautarnahli@hotmail.es>
2021-07-19 13:27:04 +0100

    same plot using the features with highest correlation

--- linear_reg_from_scratch.py
+++ linear_reg_from_scratch.py
@@ -3,13 +3,42 @@
 from sklearn import datasets
 import random
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.utils import shuffle
+import seaborn as sns
 np.random.seed(40)
-data = datasets.load_boston()
-data.keys()
-data['feature_names']
+from sklearn.datasets import load_boston
+boston_dataset = load_boston()
 X, y = datasets.load_boston(return_X_y=True)
+#Cleaning and feature engineering
+boston = pd.DataFrame(boston_dataset.data, columns=boston_dataset.feature_names)
+boston['MEDV'] = boston_dataset.target
+boston.head()
+#%%
+boston.isnull().sum()
+#%%
+sns.set(rc={'figure.figsize':(11.7,8.27)})
+sns.distplot(boston['MEDV'], bins=30)
+plt.show()
+#%%
+correlation_matrix = boston.corr().round(2)
+
+sns.heatmap(data=correlation_matrix, annot=True)
+
+plt.figure(figsize=(20, 5))
+
+features = ['LSTAT', 'RM']
+target = boston['MEDV']
+X =np.array( pd.DataFrame(np.c_[boston['LSTAT'], boston['RM']], columns = ['LSTAT','RM']))
+Y = np.array(boston['MEDV'])
+
+
+
+
+
+
+#%%
 def normalize_from_scratch(X):
     X_mean = np.mean(X, axis=0)
     X_std = np.std(X, axis=0)
@@ -40,8 +69,8 @@ def split_train_test_from_scratch(X,y,ratio):
     y_test = y[idx:]
     k=[X_train, X_test, y_train, y_test]
     #print(f'number of samples in training corresponding to {ratio}',idx)
-    for i in k:
-       print('i',i.shape)
+    # for i in k:
+    #    print('i',i.shape)
 
     return X_train, X_test, y_train, y_test
 
@@ -52,14 +81,14 @@ class DataLoader:
     def __init__(self, X, y, batch_size=16):
         self.batches = []
         idx = 0
-        X,y = shuffle_data(X,y)
+        #X,y = shuffle_data(X,y)
         while idx < len(X):
             batch = (X[idx:idx+batch_size], y[idx:idx+batch_size])
             #print('shapes in Dataloader class',batch[0].shape,batch[1].shape)
             self.batches.append(batch)
             idx += batch_size
             #print(idx)
-        np.random.shuffle(self.batches)
+        #np.random.shuffle(self.batches)
     
 
     def __getitem__(self,idx):
@@ -79,23 +108,22 @@ class LinearRegression:
         self.b = np.random.randn()
         self.list_mse_epochs = []
         self.list_mse_batches = []
-        self.idx = []
+        self.idx_x_to_plot = []
    
 
-    def fit(self,X, y) :
+    def fit_mini_batches(self,X, y) :
         learning_rate = 0.003
         #divide dataset in batches of 16 samples from the dataset
         dataloader = DataLoader(X,y,16)
         self.list_mse_epochs = []
         self.list_mse_batches = []
-        self.idx = []
-        n = 0
+        self.idx_x = []
         for i in range(20):
             for x_batches, y_batches in dataloader:
                 #shuffle batches
                 #print('shapes before shuffling and after dividing in batches', x_batches.shape, y_batches.shape)
-                x_batches_shuffled, y_batches_shuffled = shuffle_data(x_batches, y_batches)
-
+                #x_batches_shuffled, y_batches_shuffled = shuffle_data(x_batches, y_batches)
+                x_batches_shuffled, y_batches_shuffled = x_batches, y_batches
                 #prediction for the the batches using non optimal parameters yet during training 
                 # to calculate the partial derivative descent and the loss function 
                 y_hat = self.pred(x_batches_shuffled)
@@ -106,15 +134,28 @@ class LinearRegression:
                 self.w = self.w - learning_rate * grad_w
                 self.b = self.b - learning_rate * grad_b
                 #calculating and appending loss function for each batch 
-                n+=1
                 self.list_mse_batches.append(self._mse(y_batches_shuffled,y_hat))
                 #print(n,i, self._mse(y_hat,y_batches_shuffled))
-            #calculating and appending loss function for each epoch
+            #calculating and appending the mean of loss function for each epoch
             self.list_mse_epochs.append(np.mean(self.list_mse_batches[-32:]))
-            self.idx.append(len(self.list_mse_batches))
+            self.idx_x_to_plot.append(len(self.list_mse_batches))
             #print(self._mse(y_hat,y_batches) , 'epoch', i)
-       
+    def plot_loss_while_training(self):git
+        plt.plot(self.idx_x_to_plot, self.list_mse_epochs, color='red')
+        plt.plot(range(len(self.list_mse_batches)), self.list_mse_batches)
+
     
+    def fit(self,X,y):
+        learning_rate = 0.005
+        for i in range (60):
+            y_hat = self.pred(X)
+            # calculate the partial derivative
+            grad_w = 2 * (y_hat - y) @ X
+            grad_b = 2 * np.mean(y_hat-y)
+            #updating parameters 
+            self.w = self.w - learning_rate * grad_w
+            self.b = self.b - learning_rate * grad_b
+
 
         
     def _mse(self, y, y_pred):
@@ -133,20 +174,15 @@ X = normalize_from_scratch(X)
 # train_loss = model._mse(y_train, y_train_pred)
 # y_pred = model.pred(X_test)
 # val_loss = model._mse(y_test, y_pred)
-# plt.plot(model.idx, model.list_mse_epochs, color='red')
-# plt.plot(range(len(model.list_mse_batches)), model.list_mse_batches)
+# )
 # print(val_loss, train_loss)
 
 
-
-
-
-
-# %%
 def plot_loss_val_vs_train_vs_training_set_size(X,y):
     val_loss_list = []
     train_loss_list = []
-    for i in range (20,len(X)):
+    for i in range (10,len(X)):
+        print(i)
         X = X[:i,:]
         y = y[:i]
         X_train, X_test, y_train, y_test = split_train_test_from_scratch(X,y,0.2)
@@ -160,10 +196,13 @@ def plot_loss_val_vs_train_vs_training_set_size(X,y):
         train_loss_list.append(train_loss)
     plt.plot(range(len(val_loss_list)), val_loss_list, color ='red', label = 'val_loss')
     plt.plot(range(len(train_loss_list)), train_loss_list, color ='blue', label = 'train_loss')
-       
+    return(train_loss_list, val_loss_list)   
 
-
+#plot fro training set should start at zero as with two points the loss should be zero as a line can pass
+#easily throuth two points.
+# the loss in both plots it is close and high so the model is underfitting
 plot_loss_val_vs_train_vs_training_set_size(X,y)
 # %%
+
 
 # %%
